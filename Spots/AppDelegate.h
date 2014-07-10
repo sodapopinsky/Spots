2c5036af7c0792167f213f0ac42201a6bdcb1fd2
Nick Spitale <nspita1@gmail.com>
2014-07-10 08:10:45 -0500

    Initial Commit

---
+++ Spots/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Spots
+//
+//  Created by Nicholas Spitale on 7/10/14.
+//  Copyright (c) 2014 NickSpitale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Spots/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Spots
+//
+//  Created by Nicholas Spitale on 7/10/14.
+//  Copyright (c) 2014 NickSpitale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
