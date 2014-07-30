5f17a695d8a23795b1a2e45d5ab2305923d55195
Nick Spitale <nspita1@gmail.com>
2014-07-29 20:19:36 -0500

    fad

--- Spots/SPCheckInAddComments.m
+++ Spots/SPCheckInAddComments.m
@@ -34,6 +34,7 @@
 -(void)viewWillAppear:(BOOL)animated{
     
     self.navigationController.navigationBarHidden = YES;
+    [comments becomeFirstResponder];
 }
 - (void)viewDidLoad
 {
@@ -80,19 +81,18 @@
  
   
 
-    UIView *addPhotoContainer = [[UIView alloc] initWithFrame:CGRectMake(self.view.frame.size.width - 90, 10, 80, 80)];
+    UIView *addPhotoContainer = [[UIView alloc] initWithFrame:CGRectMake(self.view.frame.size.width - 95,0, 95, 95)];
     [addPhotoContainer setBackgroundColor:[UIColor blackColor]];
-    [addPhotoContainer setAlpha:0.35f];
-    addPhotoContainer.layer.cornerRadius = addPhotoContainer.frame.size.width / 2;
+    [addPhotoContainer setAlpha:0.65f];
+
    // [addPhoto addTarget:self action:@selector(shouldStartCameraController) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:addPhotoContainer];
     
     addPhoto = [UIButton buttonWithType:UIButtonTypeCustom];
     [addPhoto setFrame:CGRectMake(addPhotoContainer.frame.origin.x + 5, addPhotoContainer.frame.origin.y + 5, addPhotoContainer.frame.size.width - 10, addPhotoContainer.frame.size.width - 10)];
     [addPhoto setBackgroundColor:[UIColor clearColor]];
-    addPhoto.imageView.layer.masksToBounds = YES;
-    addPhoto.imageView.layer.cornerRadius = addPhoto.frame.size.width / 2;
-    [addPhoto setImage:[UIImage imageNamed:@"AvatarPlaceholder"] forState:UIControlStateNormal];
+
+    [addPhoto setImage:[UIImage imageNamed:@"ButtonCamera"] forState:UIControlStateNormal];
     
     [addPhoto addTarget:self action:@selector(shouldStartCameraController) forControlEvents:UIControlEventTouchUpInside];
     
@@ -101,11 +101,11 @@
 
    
     
-    UIButton *buttonView = [[UIButton alloc] initWithFrame:CGRectMake(0, 95,320, 40)];
+    UIButton *buttonView = [[UIButton alloc] initWithFrame:CGRectMake(0, 95,320, 45)];
     [buttonView setBackgroundColor:[UIColor whiteColor]];
     [buttonView setAlpha:0.9f];
      [buttonView addTarget:self action:@selector(goSelectVisibility) forControlEvents:UIControlEventTouchUpInside];
-    UILabel *lbl1 = [[UILabel alloc] initWithFrame:CGRectMake(10, 10, 200, 20)];
+    UILabel *lbl1 = [[UILabel alloc] initWithFrame:CGRectMake(10, 13, 200, 20)];
     [lbl1 setText:@"Visible to 7"];
     [lbl1 setFont:[UIFont systemFontOfSize:16.0f]];
     [lbl1 setAlpha:0.8f];
@@ -116,13 +116,29 @@
     UIButton *selectVisibility = [UIButton buttonWithType:UIButtonTypeCustom];
     [selectVisibility setImage:[UIImage imageNamed:@"RightArrow"] forState:UIControlStateNormal];
     [selectVisibility addTarget:self action:@selector(goSelectVisibility) forControlEvents:UIControlEventTouchUpInside];
-     [selectVisibility setFrame:CGRectMake(295, 5, 29*.5,.5 * 57)];
+     [selectVisibility setFrame:CGRectMake(295, 7, 29*.5,.5 * 57)];
 
     [buttonView addSubview:selectVisibility];
   
    
     
-    comments = [[UITextView alloc] initWithFrame:CGRectMake(10, 145, 300, 75)];
+    
+    UIView *userAvatarContainer = [[UIView alloc] initWithFrame:CGRectMake(10,150, 60, 60)];
+    [userAvatarContainer setBackgroundColor:[UIColor blackColor]];
+    [userAvatarContainer setAlpha:0.35f];
+    userAvatarContainer.layer.cornerRadius = userAvatarContainer.frame.size.width / 2;
+    // [addPhoto addTarget:self action:@selector(shouldStartCameraController) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:userAvatarContainer];
+    
+    UIImageView *userAvatar = [[UIImageView alloc] initWithFrame:CGRectMake(userAvatarContainer.frame.origin.x + 5, userAvatarContainer.frame.origin.y + 5, userAvatarContainer.frame.size.width - 10, userAvatarContainer.frame.size.width - 10)];
+    [userAvatar setBackgroundColor:[UIColor clearColor]];
+    userAvatar.layer.masksToBounds = YES;
+    userAvatar.layer.cornerRadius = userAvatar.frame.size.width / 2;
+    [userAvatar setImage:[UIImage imageNamed:@"AvatarPlaceholder"]];
+    [self.view addSubview:userAvatar];
+    
+    
+    comments = [[UITextView alloc] initWithFrame:CGRectMake(85, 145, 215, 75)];
     [comments setBackgroundColor:[UIColor clearColor]];
     [comments setFont:[UIFont systemFontOfSize:15.0f]];
    // comments.layer.borderColor = kSPColorDarkGray.CGColor;
